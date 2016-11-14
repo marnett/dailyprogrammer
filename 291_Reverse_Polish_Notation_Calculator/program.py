2d076392c43e4fa61e08bff14ccf7e3496bc2b52
Mitchell Arnett <mitchell.arnett@gmail.com>
2016-11-14 12:11:09 -0500

    finished Reverse Polish Notation

--- 291_Reverse_Polish_Notation_Calculator/program.py
+++ 291_Reverse_Polish_Notation_Calculator/program.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 def main(argv):
 	
@@ -6,13 +7,52 @@ def main(argv):
 		file = f.read()
 
 	content = file.split()
-	print content
 	set = ['+','-','*','x','/','//','%','^','!']
+
+	#loop through content and add floats to stack
+	stack = []
 
 	for x in content:
 		if x not in set:
-			print float(x)
-
+			stack.append(float(x))
+		else:
+			current = x
+			if current == '+':
+				input1 = stack.pop()
+				input2 = stack.pop()
+				result = float(input1 + input2)
+				stack.append(result)
+			elif current == '-':
+				input1 = stack.pop()
+				input2 = stack.pop()
+				result = float(input2 - input1)
+				stack.append(result)
+			elif current == '*' or current == 'x':
+				input1 = stack.pop()
+				input2 = stack.pop()
+				result = float(input1*input2)
+				stack.append(result)
+			elif current == '/':
+				input1 = stack.pop()
+				input2 = stack.pop()
+				result = float(input2/input1)
+				stack.append(result)
+			elif current == '//':
+				print current
+			elif current == '%':
+				print current
+			elif current == '^':
+				power = stack.pop()
+				base = stack.pop()
+				result = float(base**power)
+				stack.append(result)
+			elif current == '!':
+				input1 = stack.pop()
+				result = float(math.factorial(input1))
+				stack.append(result)
+	
+	#print final result
+	print stack.pop()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
